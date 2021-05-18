81a327880023d31a75282abaa1033440ef26bb12
droplet92 <lhs1438@naver.com>
2021-05-18 18:18:15 +0900

    기본 경로 수정 및 미사용 파일 삭제

--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
 @echo off
+set root=%HOMEDRIVE%%HOMEPATH%
 echo Register send.bat to taskschd...
-schtasks /create /tn "Whitelist Sender" /sc "DAILY" /rl HIGHEST /st 00:00 /ru system /tr "%~dp0%\send.bat"
+schtasks /create /tn "Whitelist Sender" /sc "DAILY" /rl HIGHEST /st 00:00 /ru system /tr "%root%\send.bat"
 echo Install Succeeded. This window will be closed automatically after 10 seconds..."
 timeout 10 > NUL

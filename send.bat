3eccfaed428be8ca9f711ca1bc878b820c9a81b4
droplet92 <lhs1438@naver.com>
2021-05-30 23:24:41 +0900

    시연용

--- send.bat
+++ send.bat
@@ -7,7 +7,7 @@ set id_rsa_location=%root%\.ssh\id_rsa
 echo start uploading
 echo uploading whitelist
 echo ssh connects to %ssh_account%...
-set insert_query="insert into Whitelist(Version,LastDistributed) values('%DATE%',null);"
+set insert_query="insert into Whitelist(Version) values('%DATE%');"
 ssh -i %id_rsa_location% %ssh_account% "mkdir Whitelists & sqlite3 %root%\Desktop\UHDControlServer\db.sqlite \"%insert_query%\""
 scp -i %id_rsa_location% "%root%\Whitelists\%DATE%.sqlite" "%ssh_account%:Whitelists"
 echo.
--- setup.bat
+++ setup.bat
@@ -1,6 +1,9 @@
 @echo off
 set root=%HOMEDRIVE%%HOMEPATH%
+echo 1. Copy send.bat to %root%...
+set SOURCE_PATH=%~dp0%send.bat
+copy "%SOURCE_PATH%" "%root%"
 echo Register send.bat to taskschd...
-schtasks /create /tn "Whitelist Sender" /sc "DAILY" /rl HIGHEST /st 00:00 /ru system /tr "%root%\send.bat"
+schtasks /create /tn "Whitelist Sender" /sc "DAILY" /rl HIGHEST /st 13:00 /tr "%root%\send.bat"
 echo Install Succeeded. This window will be closed automatically after 10 seconds..."
 timeout 10 > NUL
